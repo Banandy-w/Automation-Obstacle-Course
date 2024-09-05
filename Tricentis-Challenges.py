8b35a136696503ddd78b68f147cb3060274232cd
Andy <anwong16@gmail.com>
2024-09-05 12:09:02 -0700

    Put tests into individual methods so they can be easily commented out and tested in the future. Made check_success return Test Success and Test Failed

--- Tricentis-Challenges.py
+++ Tricentis-Challenges.py
@@ -4,26 +4,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
+
+import time
 
 
 #After succeeding in an obstacle, the browser dynamically changes with a pop up for success
 #This checks that the test has succeeded and then clicks try again to stay on the page.
+#Returns 'Test Success' or 'Test Failed'
 def check_success(browser):
     try:
         wait = WebDriverWait(browser, 10)
         dynamicButton = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,'.sweet-alert > h2:nth-child(6)')))
         assert dynamicButton.text == "Good job!"
-        print('Test success!')
+        
         dynamicButton = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'cancel'))).click()
+        return 'Test Success'
 
     except AssertionError:
         print('Test failed. Please check results')
+        return 'Test Failed'
 
-def main():
-    browser = webdriver.Firefox()
+def TEST_ids_not_everything(browser):
+    
     browser.get('https://obstaclecourse.tricentis.com/Obstacles/22505')
     wait = WebDriverWait(browser, 10)
 
@@ -32,16 +36,21 @@ def main():
 
     #Solution use xpath but make it more specific by including the text of the button
     print("Current obstacle EASY: Ids are not everything")
+    print('Click on the button that says "Click me!"')
     element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="dontuseid" and text()="Click me!"]'))).click()
     time.sleep(2)
-    check_success(browser)
 
-    
+    result = check_success(browser)
+    print(result)
+    return result
+
+def TEST_testing_methods(browser):
     #Challenge select multiple items in a multiselect form. One of the elements is out of view in the scrollbox
     #Solution: define a var for each element needed to be selected. Then use action chains to similate control clicking.
     browser.get('https://obstaclecourse.tricentis.com/Obstacles/94441')
     print('Current obstacle HARD: Testing methods. ')
-    print(' Multiselect listbox: Select all the testing methods, that are supported by Tosca (Functional, End2End, GUI testing and Exploratory Testing) ')
+    print('Multiselect listbox: Select all the testing methods, that are supported by Tosca (Functional, End2End, GUI testing and Exploratory Testing) ')
+    wait = WebDriverWait(browser, 10)
     element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#multiselect')))
 
     ele_functional = browser.find_element(By.CSS_SELECTOR,'#multiselect > option:nth-child(1)')
@@ -56,8 +65,38 @@ def main():
     ele_exploratory = browser.find_element(By.CSS_SELECTOR,'#multiselect > option:nth-child(5)')
     browser.execute_script("arguments[0].scrollIntoView();", ele_E2E)
     ActionChains(browser).key_down(Keys.CONTROL).click(ele_exploratory).key_up(Keys.CONTROL).perform()
+    
     time.sleep(2)
-    check_success(browser)
+    result = check_success(browser)
+    print(result)
+    return result
+
+def TEST_not_a_table(browser):
+    browser.get('https://obstaclecourse.tricentis.com/Obstacles/64161')
+    wait = WebDriverWait(browser, 10)
+    element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#generate'))).click
+
+    print('Current Obstacle HARD: Not a table')
+    print('Click on Generate Order ID, takes the generated ID and enters it into the edit box. The generated id is randomly ordered in a table')
+
+
+
+    time.sleep(2)
+    result = check_success(browser)
+    print(result)
+    return result
+
+def main():
+    browser = webdriver.Firefox()
+    wait = WebDriverWait(browser, 10)
+    
+    #TEST_ids_not_everything(browser)
+    #TEST_testing_methods(browser)
+    TEST_not_a_table(browser)
+
+
+    
+
 
 
 
